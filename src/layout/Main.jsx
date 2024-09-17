853408dc4340ddf9c5ff2e88dee1ab00687721f6
Arif Hossain <arif.raj9911@gmail.com>
2024-09-18 03:21:01 +0600

    sidebar and outlet element scroll problem solved

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,12 +4,12 @@ import Sidebar from "../Pages/Shared/Sidebar/Sidebar";
 const Main = () => {
   return (
     <div className="flex min-h-screen max-w-screen-mak mx-auto">
-      <div className="2xl:w-[16%] xl:w-[18%] w-1/5  bg-[#F9F9F9] sticky top-0 min-h-screen ">
-          <Sidebar />
-        </div>
-        <div className="flex-grow overflow-auto w-full bg-[#FFF] pl-9 py-8 pr-20">
-          <Outlet></Outlet>
-        </div>
+      <div className="2xl:w-[16%] xl:w-[18%] w-1/5  bg-[#F9F9F9]   min-h-screen max-h-screen overflow-y-auto sticky top-0 sidebar-scrollbar">
+        <Sidebar />
+      </div>
+      <div className="flex-grow  w-full bg-[#FFF] pl-9 py-8 pr-20 max-h-screen overflow-y-auto sticky top-0">
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
