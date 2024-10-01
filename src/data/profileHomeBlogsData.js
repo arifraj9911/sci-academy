63770d8d08226b1db4f19bf971240a93a9159031
Amin7775 <amin.dpi24@gmail.com>
2024-10-01 22:01:11 +0600

    Fixed search functionality from profile home page

--- src/data/profileHomeBlogsData.js
+++ src/data/profileHomeBlogsData.js
@@ -2,124 +2,160 @@ import blogPicture from "./../assets/images/blogPicture.png";
 
 const blogPostsData = [
   {
-    id: 1,
-    date: "15 Aug 2022",
-    category: "Career Tips",
+    id: 1, // Unique ID for each blog post
+    subject: "Career Tips",
+    author_id: "student", // Placeholder, replace with actual user ID
     title: "How To Explain Why You're Right For The Job",
-    description:
+    short_description:
       "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    content:
+      "Detailed content about explaining why you're the right fit for the job goes here.",
+    uploaded_on: new Date("2022-08-15").getTime(), // Convert date to timestamp
+    videos: [], // No videos for now
+    images: [blogPicture], // Use placeholder image
+    likes: 1,
   },
   {
     id: 2,
-    date: "15 Aug 2022",
-    category: "Career Tips",
+    subject: "Career Tips",
+    author_id: "student",
     title: "How To Highlight Your Skills in a Resume",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus error, dignissimos deserunt culpa eaque sapiente reiciendis aperiam quasi aliquam accusantium, iusto sint, molestias neque sunt! Ipsum repellat quia nesciunt quod, voluptates amet nisi sed exercitationem eum molestias totam reprehenderit vel beatae nihil tempore pariatur saepe. Vero aperiam temporibus nesciunt quibusdam, voluptate minima, nulla dolorum maxime repellendus magni officiis ad error, cum odit iusto ex voluptatem accusantium natus sed repellat. Ipsum omnis, incidunt neque error atque maxime inventore perspiciatis iure dignissimos dolores minus sed quisquam esse modi? Quod adipisci, impedit laboriosam corporis quaerat quasi laudantium officia tempora repellat deleniti ducimus eaque.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Learn the best ways to highlight your skills in your resume for maximum impact.",
+    content: "Full content about highlighting skills in a resume goes here.",
+    uploaded_on: new Date("2022-08-15").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 3,
-    date: "18 Sep 2022",
-    category: "Interview",
+    subject: "Interview",
+    author_id: "student",
     title: "Mastering Behavioral Interview Questions",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content about mastering behavioral interview questions.",
+    uploaded_on: new Date("2022-09-18").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 4,
-    date: "20 Sep 2022",
-    category: "Interview",
+    subject: "Interview",
+    author_id: "student",
     title: "Top Questions to Ask at the End of an Interview",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description: "Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content about top questions to ask during an interview.",
+    uploaded_on: new Date("2022-09-20").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 5,
-    date: "25 Oct 2022",
-    category: "Work From Home",
+    subject: "Work From Home",
+    author_id: "student",
     title: "Effective Tips for Working from Home",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description: "Tips on working effectively from home.",
+    content: "Detailed content about effective work-from-home tips.",
+    uploaded_on: new Date("2022-10-25").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 6,
-    date: "30 Oct 2022",
-    category: "Work From Home",
+    subject: "Work From Home",
+    author_id: "student",
     title: "Setting Up a Productive Home Office",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content about setting up a productive home office space.",
+    uploaded_on: new Date("2022-10-30").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 7,
-    date: "10 Nov 2022",
-    category: "Success Story",
+    subject: "Success Story",
+    author_id: "student",
     title: "How I Landed My Dream Job in Tech",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content on landing a dream job in tech.",
+    uploaded_on: new Date("2022-11-10").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 8,
-    date: "20 Nov 2022",
-    category: "Success Story",
+    subject: "Success Story",
+    author_id: "student",
     title: "From Freelancer to Full-Time: My Journey",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content:
+      "A success story about transitioning from freelancing to full-time employment.",
+    uploaded_on: new Date("2022-11-20").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 9,
-    date: "01 Dec 2022",
-    category: "Courses",
+    subject: "Courses",
+    author_id: "student",
     title: "Top Online Courses to Upskill in 2023",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content on top online courses to upskill in 2023.",
+    uploaded_on: new Date("2022-12-01").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 10,
-    date: "10 Dec 2022",
-    category: "Courses",
+    subject: "Courses",
+    author_id: "student",
     title: "Best Certifications to Boost Your Career",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content about the best certifications to boost your career.",
+    uploaded_on: new Date("2022-12-10").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 11,
-    date: "15 Dec 2022",
-    category: "Exam Tips",
+    subject: "Exam Tips",
+    author_id: "student",
     title: "How to Prepare for Online Exams",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content about preparing for online exams.",
+    uploaded_on: new Date("2022-12-15").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
   {
     id: 12,
-    date: "20 Dec 2022",
-    category: "Exam Tips",
+    subject: "Exam Tips",
+    author_id: "student",
     title: "Time Management Tips for Exam Day",
-    description:
-      "Lorem ipsum dolor sit amet consectetur. Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
-    image: blogPicture,
-    readMoreLink: "#",
+    short_description:
+      "Velit et eget nam lorem. Imperdiet in adipiscing venenatis purus vitae.",
+    content: "Content on time management for exams.",
+    uploaded_on: new Date("2022-12-20").getTime(),
+    videos: [],
+    images: [blogPicture],
+    likes: 1,
   },
 ];
 
