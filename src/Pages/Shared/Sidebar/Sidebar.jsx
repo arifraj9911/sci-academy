f5f6ea175cc7aada1dec23238868af0257b37dd2
Arif Hossain <arif.raj9911@gmail.com>
2024-10-10 19:47:26 +0600

    route added to the profile logo and user logo

--- src/Pages/Shared/Sidebar/Sidebar.jsx
+++ src/Pages/Shared/Sidebar/Sidebar.jsx
@@ -3,19 +3,23 @@
 // import academy from "../../../assets/images/Academy.svg";
 import logo from "./../../../assets/images/logo2.svg";
 import NavLinkItem from "../../../components/NavLinkItem";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
   return (
     <div className="py-6">
       {/* logo */}
-      <div className="flex items-center justify-center gap-2">
+      <div
+        onClick={() => navigate("/user/home")}
+        className="flex cursor-pointer items-center justify-center gap-2"
+      >
         <img src={logo} alt="" />
         {/* <img src={logo} alt="logo" />
         <div className="space-y-1">
           <img src={science} alt="science" />
           <img src={academy} alt="academy" />
         </div> */}
-
       </div>
 
       {/* menu Link */}
