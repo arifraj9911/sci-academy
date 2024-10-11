482ea3b37a6e8a4726c1ab0284b1f7a24fadc942
kabir4874 <kabir.cse.bd@gmail.com>
2024-10-11 15:22:12 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
 import { router } from "./Routes/Routes.jsx";
-import AuthProvider from "./provider/AuthProvider.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
